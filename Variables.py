64e473695b61bf96883a943da72b557dc9f8371e
eduochoa-dev <127692180+ochoaedu@users.noreply.github.com>
2025-11-20 20:13:09 -0600

    Create Variables.py
    This is my second lesson of python where I learned how to define variables and a bit of operators.

---
+++ Variables.py
@@ -0,0 +1,16 @@
+#Variables in python are like boxes where you can store different data types
+#but those values can change during the program execution
+
+name = "Eduardo" #this is a variable definition
+
+string = "Welcome to Python"
+
+phrase = string + " " + name #using the + symbol means concatenation of the values contained in the variables. 
+print(phrase)
+
+
+num1 = 5
+
+num2 = 7
+
+print(num1+num2) #but the + character can also be used as an addition operator. 
